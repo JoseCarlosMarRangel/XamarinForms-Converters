ad05ec09d2e1a191081c64c98e34396302569a7b
José Carlos Mar Rangel <68357760+JoseCarlosMarRangel@users.noreply.github.com>
2022-10-01 14:19:31 -0500

    basedeproyecto

---
+++ ConverterEjemplo/EdadConverter.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Globalization;
+using Xamarin.Forms;
+
+namespace ConverterEjemplo 
+{
+    public class EdadConverter : IValueConverter
+    {
+        // ! Metodo que convierte el valor entregando el resultado y que recibe 4 parametros
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+
+            string text = $"{value}";
+            if (!string.IsNullOrEmpty(text))
+            {
+                if (int.Parse(text) >= 18)
+                {
+                    return "Eres mayor de edad";
+                }
+                else
+                {
+                    return "Eres menor de edad";
+                }
+            }
+            return string.Empty;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return string.Empty;
+        }
+    }
+}
---
+++ ConverterEjemplo/EnanleButtonByEdad.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Globalization;
+using Xamarin.Forms;
+
+namespace ConverterEjemplo 
+{
+    public class EnanleButtonByEdad : IValueConverter
+    {
+        // ! Metodo que convierte el valor entregando el resultado y que recibe 4 parametros
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+
+            string text = $"{value}";
+            if (!string.IsNullOrEmpty(text))
+            {
+                if (int.Parse(text) >= 18)
+                {
+                    return true;
+                }
+                else
+                {
+                    return false;
+                }
+            }
+            return false;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return false;
+        }
+    }
+}
